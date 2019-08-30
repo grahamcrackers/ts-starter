975948bb10dfc02383fe72f1156caa9adbf40c08
Graham Rogers <gmrogers04@gmail.com>
2019-08-29 20:46:58 -0500

    preferred base implementation

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+// https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    extends: [
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        // 'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        // 'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    parserOptions: {
+        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+        tsconfigRootDir: "./"
+    },
+    rules: {
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        
+        // '@typescript-eslint/interface-name-prefix': 'always',
+        // '@typescript-eslint/no-explicit-any': false,
+        // '@typescript-eslint/explicit-function-return-type': false,
+        // '@typescript-eslint/no-object-literal-type-assertion': ["error", {
+        //   allowAsParameter: true // Allow type assertion in call and new expression, default false
+        // }],
+        // '@typescript-eslint/explicit-member-accessibility': false
+    },
+};
